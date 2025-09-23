f81d5ac49c26b342909a0dcf505688d2aa50fb44
Ajay <ajaykathar30gmail.com>
2025-09-23 16:29:00 +0530

    added .env to gitignore

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -67,8 +67,11 @@ export const login = async (req, res) => {
             phoneNumber: user.phoneNumber,
             profile: user.profile
         }
-        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' })
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'None',secure:true }).json({ message: `Welcome ${(user.name).toUpperCase()}`, success: true, user })
+
+        const token = jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' })
+        return res.status(200)
+                  .cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'None',secure:true })
+                  .json({ message: `Welcome ${(user.name).toUpperCase()}`, success: true, user })
 
     }
 
