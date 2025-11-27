f7067f9d230b5c90def752fb429dabcae3e62c3e
Ajay <ajaykathar30gmail.com>
2025-11-27 17:09:11 +0530

    added deployed frontend url in allowedOrigins

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,8 @@ import applicationRoute from './routes/application.route.js';
 
 dotenv.config({});
 
-const allowedOrigins = ['http://localhost:5173'];
-// const allowedOrigins = ['http://localhost:5173', 'https://jobportalapp-1.onrender.com'];
+// const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://jobify-frontend-4lxx.onrender.com'];
 
 const corsOptions = {
   origin: function (origin, callback) {
